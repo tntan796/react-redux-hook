8a53ba8d0949fa942e58456fc2bf278ba8b44913
Tan Tano <tantn1@sunshinegroup.vn>
2021-06-19 11:04:25 +0700

    get task from redux

--- src/commons/constants.ts
+++ src/commons/constants.ts
@@ -3,3 +3,9 @@ export const TaskStatus = {
     IN_PROGRESS: 'in progress',
     SUCCESS: 'success'
 }
+
+export const ACTION_TYPE = {
+    LIST_ALL: 'list all',
+    SEARCH_TASK: 'search task',
+    FILTER_TASK: 'filter task'
+}
